11e96ce5e7e11ddddbd8598fa92817c2833b49b7
001-mohammad-anwar <mohammadanwarofficial2@gmail.com>
2025-12-25 21:25:45 +0530

    remove footer for Now we will add footer later

--- src/App.jsx
+++ src/App.jsx
@@ -136,7 +136,7 @@ const fetchUser = async () => {
         <main className=" min-h-[80vh] ">
           <Outlet />
         </main>
-        <Footer />
+        {/* <Footer /> */}
         <Toaster />
 
         {
